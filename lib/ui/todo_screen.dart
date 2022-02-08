d24cd02e595cee9dd541f53462ea5d33a8a85a51
WingKit <mfkenson@gmail.com>
2022-02-09 02:50:06 +0800

    fix deprecated FlatButton, showSnackBar

--- lib/ui/todo_screen.dart
+++ lib/ui/todo_screen.dart
@@ -12,93 +12,95 @@ class TodoScreen extends StatefulWidget {
 
 class _TodoScreenState extends State<TodoScreen> {
   TextEditingController _taskController = TextEditingController();
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
       appBar: AppBar(
-        title: Text("Todo Screen"),
+        title: Text("#TODO"),
       ),
       body: Container(
-        child: FutureBuilder(builder: (context, snapshot) {
-          if (snapshot.data != null) {
-            List<Todo> todoList = snapshot.data;
-
-            return ListView.builder(itemBuilder: (_, position) {
-              return ListTile(
-                title: Text(todoList[position].task),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    IconButton(icon: Icon(Icons.edit), onPressed: () {
-                      showUpdateDialog(todoList[position]);
-                    }),
-                    IconButton(icon: Icon(Icons.check_circle, color: Colors.green,), onPressed: () {
-                      deleteTodo(todoList[position].objectId);
-                    })
-                  ],
-                ),
+        child: FutureBuilder(
+          builder: (context, snapshot) {
+            if (snapshot.data != null) {
+              List<Todo> todoList = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (_, position) {
+                  return ListTile(
+                    title: Text(todoList[position].task),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              showUpdateDialog(todoList[position]);
+                            }),
+                        IconButton(
+                            icon: Icon(
+                              Icons.check_circle,
+                              color: Colors.green,
+                            ),
+                            onPressed: () {
+                              deleteTodo(todoList[position].objectId);
+                            })
+                      ],
+                    ),
+                  );
+                },
+                itemCount: todoList.length,
               );
-            },
-              itemCount: todoList.length,
-            );
-
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
           future: getTodoList(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        showAddTodoDialog();
-      },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showAddTodoDialog();
+        },
         child: Icon(Icons.add),
       ),
     );
   }
 
-
-
   void showUpdateDialog(Todo todo) {
-
     _taskController.text = todo.task;
 
-    showDialog(context: context,
-      builder: (_) => AlertDialog(
-        content: Container(
-          width: double.maxFinite,
-          child: TextField(
-            controller: _taskController,
-            decoration: InputDecoration(
-              labelText: "Enter updated task",
-            ),
-          ),
-        ),
-        actions: <Widget>[
-          FlatButton(onPressed: () {
-            Navigator.pop(context);
-            todo.task = _taskController.text;
-            updateTodo(todo);
-          }, child: Text("Update")),
-          FlatButton(onPressed: () {
-            Navigator.pop(context);
-          }, child: Text("Cancel")),
-        ],
-      )
-    );
-
-
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              content: Container(
+                width: double.maxFinite,
+                child: TextField(
+                  controller: _taskController,
+                  decoration: InputDecoration(
+                    labelText: "Enter updated task",
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      todo.task = _taskController.text;
+                      updateTodo(todo);
+                    },
+                    child: Text("Update")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancel")),
+              ],
+            ));
   }
 
-
-  Future <List<Todo>> getTodoList() async{
-
+  Future<List<Todo>> getTodoList() async {
     List<Todo> todoList = [];
 
     Response response = await TodoUtils.getTodoList();
@@ -112,7 +114,6 @@ class _TodoScreenState extends State<TodoScreen> {
       for (var todo in results) {
         todoList.add(Todo.fromJson(todo));
       }
-
     } else {
       //Handle error
     }
@@ -120,150 +121,122 @@ class _TodoScreenState extends State<TodoScreen> {
     return todoList;
   }
 
-
-
-
   void showAddTodoDialog() {
-
-    showDialog(context: context,
-      builder: (_) => AlertDialog(
-        content: Container(
-          width: double.maxFinite,
-          child: TextField(
-            controller: _taskController,
-            decoration: InputDecoration(
-              labelText: "Enter task",
-            ),
-          ),
-        ),
-        actions: <Widget>[
-          FlatButton(onPressed: () {
-
-            Navigator.pop(context);
-            addTodo();
-
-          }, child: Text("Add")),
-          FlatButton(onPressed: () {
-            Navigator.pop(context);
-          }, child: Text("Cancel")),
-        ],
-      )
-    );
-
+    _taskController.text = "";
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              content: Container(
+                width: double.maxFinite,
+                child: TextField(
+                  controller: _taskController,
+                  decoration: InputDecoration(
+                    labelText: "Enter task",
+                  ),
+                ),
+              ),
+              actions: <Widget>[
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      addTodo();
+                    },
+                    child: Text("Add")),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancel")),
+              ],
+            ));
   }
 
   void addTodo() {
-
-    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Row(
-      children: <Widget>[
-        Text("Adding task"),
-        CircularProgressIndicator(),
-      ],
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    ),
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Row(
+        children: <Widget>[
+          Text("Adding task"),
+          CircularProgressIndicator(),
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      ),
       duration: Duration(minutes: 1),
     ));
 
     Todo todo = Todo(task: _taskController.text);
 
-    TodoUtils.addTodo(todo)
-    .then((res) {
-
-      _scaffoldKey.currentState.hideCurrentSnackBar();
+    TodoUtils.addTodo(todo).then((res) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
 
       Response response = res;
       if (response.statusCode == 201) {
         //Successful
         _taskController.text = "";
 
-        _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text("Todo added!"), duration: Duration(seconds: 1),));
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text("Saved!"),
+          duration: Duration(seconds: 1),
+        ));
 
-        setState(() {
-
-        });
-
+        setState(() {});
       }
-
     });
-
   }
 
-
   void updateTodo(Todo todo) {
-
-    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Text("Updating todo"),
-        CircularProgressIndicator(),
-      ],
-    ),
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Row(
+        children: <Widget>[
+          Text("Updating"),
+          CircularProgressIndicator(),
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      ),
       duration: Duration(minutes: 1),
-    ),);
+    ));
 
-
-    TodoUtils.updateTodo(todo)
-    .then((res) {
-
-      _scaffoldKey.currentState.hideCurrentSnackBar();
-
+    TodoUtils.updateTodo(todo).then((res) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
       Response response = res;
       if (response.statusCode == 200) {
-        //Successfully Deleted
-        _taskController.text = "";
-        _scaffoldKey.currentState.showSnackBar(SnackBar(content: (Text("Updated!"))));
         setState(() {
-
+          _taskController.text = "";
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("Updated!"),
+            duration: Duration(seconds: 1),
+          ));
         });
       } else {
         //Handle error
       }
     });
-
-
   }
-
-
-
 
   void deleteTodo(String objectId) {
-
-    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Text("Deleting todo"),
-        CircularProgressIndicator(),
-      ],
-    ),
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Row(
+        children: <Widget>[
+          Text("Deleting"),
+          CircularProgressIndicator(),
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      ),
       duration: Duration(minutes: 1),
-    ),);
+    ));
 
-
-    TodoUtils.deleteTodo(objectId)
-      .then((res) {
-
-        _scaffoldKey.currentState.hideCurrentSnackBar();
-
-        Response response = res;
-        if (response.statusCode == 200) {
-          //Successfully Deleted
-          _scaffoldKey.currentState.showSnackBar(SnackBar(content: (Text("Deleted!")),duration: Duration(seconds: 1),));
-          setState(() {
-
-          });
-        } else {
-          //Handle error
-        }
+    TodoUtils.deleteTodo(objectId).then((res) {
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+      if (res.statusCode == 200) {
+        //Successfully Deleted
+        setState(() {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("Deleted!"),
+            duration: Duration(seconds: 1),
+          ));
+        });
+      } else {
+        //Handle error
+      }
     });
-
   }
-
-
-
-
-
 }
-
-
-
-
