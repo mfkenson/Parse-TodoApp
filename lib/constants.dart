53691ef07b253b9d3826ccdee83d944785a1f5cb
WingKit <mfkenson@gmail.com>
2022-02-09 09:30:04 +0800

    ignore apikey constants

--- lib/constants.dart
+++
@@ -1,3 +0,0 @@
-//Todo : Paste the credentials from your project console in back4app.com
-const kParseApplicationId = "0DPvO1zlZQI5wQpVx7KjljRPfq4i5jpumvTVyuTc";
-const kParseRestApiKey = "8cOsOJL2wXNxhR4eL84eN3oXmaGGYsjEoiuRji9f";
